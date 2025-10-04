73d57258aa72e270b8c09c9af6044dcc9e161f90
Eren <erenbas3453@gmail.com>
2025-10-04 18:38:10 +0300

    Fix error in gold price API call

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,19 +4,30 @@ import products from "@/public/products.json";
 export async function GET(req: Request) {
   const goldPrice = await getGoldPrice();
 
-  const enriched = products.map((p) => {
+  const productList = products.map((p) => {
     const price = (p.popularityScore + 1) * p.weight * goldPrice;
     return { ...p, price };
   });
 
-  return NextResponse.json(enriched);
+  return NextResponse.json(productList);
 }
 
 async function getGoldPrice() : Promise<number> {
     try {
-    const res = await fetch(`https://www.goldapi.io/api/XAU/USD`);
-    const data = await res.json();
-    return data[0].price_gram_10k;
+      let header = new Headers();
+      header.append("x-access-token", process.env.GOLD_API_KEY!);
+      header.append("Content-Type", "application/json");
+
+      let requestOptions = {
+        method: 'GET',
+        headers: header,
+        };
+
+      const res = await fetch(`https://www.goldapi.io/api/XAU/USD`, requestOptions);
+      const data = await res.json();
+
+      return data.price_gram_10k;
+
   } catch {
     return 60;
   }
