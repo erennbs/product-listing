f871251ebc89499686aa0442544245509f20bef7
Eren <erenbas3453@gmail.com>
2025-10-04 16:41:14 +0300

    Bug fix for Slider
    - When slide per view set auto it only showed one. Set SwipperSlide width to auto in css

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -13,7 +13,7 @@ export default function ProductCard({ product }: ProductCardProps) {
   return (
     <div className="bg-white rounded-2xl p-4 w-[300px] h-[400px]">
       <div className="mt-4">
-        <img src={product.images[selectedImage]} alt="" className="w-full rounded-2xl"/>
+        <img src={product.images[selectedImage]} alt="ring" className="w-full rounded-2xl"/>
         <h2 className="text-lg font-semibold mt-3">{product.name}</h2>
         <p className="text-gray-700 mb-5">${product.price.toFixed(2)}</p>
         <ColorPicker
