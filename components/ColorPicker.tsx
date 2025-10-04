20806e5c364a53383b6cfd8f135a095615b911f3
Eren <erenbas3453@gmail.com>
2025-10-04 16:33:47 +0300

    Add custom colors for ColorPicker

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -1,17 +1,24 @@
+import { useEffect } from "react";
+
 interface ColorPickerProps {
     colors: {[key: string]: string}
     onSelect: (img: string) => void
 }
 
 export default function ColorPicker({ colors, onSelect }: ColorPickerProps) {
+  const backgroundColorMap: {[key: string]: string} = {
+    "rose": "bg-rose-gold",
+    "white": "bg-white-gold",
+    "yellow": "bg-yellow-gold"
+  }
+  
   return (
-    <div className="flex gap-2 mt-2">
-      {Object.keys(colors).map((key, i) => (
+    <div className="flex gap-2">
+      {Object.keys(colors).map((color, i) => (
         <button
           key={i}
-          className="w-6 h-6 rounded-full border"
-          style={{ backgroundColor: "red" }}
-          onClick={() => onSelect(key)}
+          className={`w-6 h-6 rounded-full ${backgroundColorMap[color]}`}
+          onClick={() => onSelect(color)}
         />
       ))}
     </div>
