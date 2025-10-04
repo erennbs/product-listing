7c226de58d83321f2e33d089c834cfbf8cb8fc69
Eren <erenbas3453@gmail.com>
2025-10-04 17:04:55 +0300

    Add outline to selected color

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 
 interface ColorPickerProps {
+    selected: string
     colors: {[key: string]: string}
     onSelect: (img: string) => void
 }
 
-export default function ColorPicker({ colors, onSelect }: ColorPickerProps) {
+export default function ColorPicker({ selected, colors, onSelect }: ColorPickerProps) {
   const backgroundColorMap: {[key: string]: string} = {
     "rose": "bg-rose-gold",
     "white": "bg-white-gold",
@@ -17,7 +18,7 @@ export default function ColorPicker({ colors, onSelect }: ColorPickerProps) {
       {Object.keys(colors).map((color, i) => (
         <button
           key={i}
-          className={`w-6 h-6 rounded-full ${backgroundColorMap[color]}`}
+          className={`w-5 h-5 rounded-full ${selected == color && "outline outline-offset-3"} ${backgroundColorMap[color]}`}
           onClick={() => onSelect(color)}
         />
       ))}
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -11,12 +11,13 @@ export default function ProductCard({ product }: ProductCardProps) {
   const [selectedImage, setSelectedImage] = useState("yellow");
 
   return (
-    <div className="bg-white rounded-2xl p-4 w-[300px] h-[400px]">
+    <div className="bg-white rounded-2xl p-4 w-[280px] h-[400px]">
       <div className="mt-4">
         <img src={product.images[selectedImage]} alt="ring" className="w-full rounded-2xl"/>
         <h2 className="text-lg font-semibold mt-3">{product.name}</h2>
         <p className="text-gray-700 mb-5">${product.price.toFixed(2)}</p>
         <ColorPicker
+          selected={selectedImage}
           colors={product.images}
           onSelect={(img: string) => setSelectedImage(img)}
         />
